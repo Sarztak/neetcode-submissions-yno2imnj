f7a90010f6760596b84d381db56b2eda76876c2f
Sarthak Sunil Dhanke <110623549+Sarztak@users.noreply.github.com>
2026-04-04 08:53:05 -0500

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[0] = 0
+
+        for i in range(1, n):
+            for k in range(1, i + 1):
+                if k <= nums[i - k]: # this means the distance between i and i - k which is k <= what I can jump from nums[i - k]; this is the contraint of the problems
+                    dp[i] = min(dp[i], dp[i - k] + 1)
+        
+        return dp[-1]
+
+
