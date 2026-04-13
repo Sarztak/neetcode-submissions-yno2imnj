1d54836486e1b6dff4e1189036c81de5cf73299f
Sarthak Sunil Dhanke <110623549+Sarztak@users.noreply.github.com>
2026-04-13 16:47:53 -0500

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+
+        if lo == hi and target == nums[0]:
+            return 0
+        
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                # this means that all the number from index lo to mid are less than target because 
+                # the array is sorted, therefore I can reduce the search space
+                lo = mid + 1
+            else:
+                # this means nums[mid] > target so all the elements from index mid to hi are greater
+                # the target since array is sorted, therefore I can reduce the search space
+                hi = mid - 1
+        
+        return -1
+                
+        
