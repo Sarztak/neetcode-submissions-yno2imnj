7b9eaac1787924b7dbd30d07a129c0dc187974ba
Sarthak Sunil Dhanke <110623549+Sarztak@users.noreply.github.com>
2026-04-19 12:16:01 -0500

    Add: search-in-rotated-sorted-array-ii - submission-3

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-3.py
@@ -0,0 +1,83 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        """
+        I spend a lot of time thinking that there is a log(n) solution but there isn't
+        Having duplicate elements breaks the conditions required for finding the minimum element
+        in log(n) time complexity so this case at worst will degenerate into O(n) when all the elements are same 
+        So I had to add a extra cases to handle the ambiguity when arr[lo] == arr[mid] == arr[hi] 
+        """
+        def binary_search(lo, hi, target):
+            while lo <= hi:
+                mid = (lo + hi) // 2
+
+                if nums[mid] == target:
+                    return True 
+                elif nums[mid] > target:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            return False 
+
+        n = len(nums)
+        lo = 0
+        hi = n - 1
+
+        def find_min_index(lo, hi):
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if lo == hi:
+                    return lo
+                elif hi == lo + 1:
+                    # return the index that has minimum if they are not equal else smaller of the two indices
+                    if nums[lo] <= nums[hi]:
+                        return lo
+                    else:
+                        return hi
+                elif nums[lo] == nums[mid] == nums[hi]:
+                    lo = lo + 1
+                elif nums[lo] <= nums[mid] < nums[hi]:
+                    hi = mid
+                elif nums[lo] < nums[mid] <= nums[hi]:
+                    lo = mid
+                elif nums[lo] < nums[mid] < nums[hi]:
+                    return lo
+                elif nums[lo] <= nums[mid] and nums[mid] > nums[hi]:
+                    lo = mid 
+                elif nums[mid] <= nums[hi] and nums[lo] > nums[mid]:
+                    hi = mid
+
+            return -1 
+        
+        min_index = find_min_index(lo, hi)
+
+        assert min_index != -1, "min_index not found" 
+        
+        if min_index != -1:
+            t1 = binary_search(min_index, n - 1, target)
+            t2 = binary_search(0, min_index - 1, target)
+
+            return t1 or t2
+        else:
+            return False
+
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
