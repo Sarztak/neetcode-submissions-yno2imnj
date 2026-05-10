83d4f7747fbfef23cb06148c885c4e1a44be4410
Sarthak Sunil Dhanke <110623549+Sarztak@users.noreply.github.com>
2026-05-09 22:04:22 -0500

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        aux = [0] * n
+
+        """
+        The plan is as follows the aux array is filled with zero, if any 
+        positive number occurs I will place it in that aux if there is space
+        so aux contain 1, 2, 3, ... at index 0, 1, 2... now if there is any 
+        gap meaning the first index in the aux which is still zero will be
+        the answer. In case aux is full then answer is n + 1. This is still
+        not constant space because the length of aux is equal to that of nums. 
+        """
+
+        for k in nums:
+            if 1 <= k <= n:
+                aux[k - 1] = k
+        
+        for i in range(n):
+            if aux[i] == 0:
+                return i + 1
+        
+        return n + 1
+        
+
+
+
