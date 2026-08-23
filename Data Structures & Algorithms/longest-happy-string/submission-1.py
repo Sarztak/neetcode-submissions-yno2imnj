bdfcb6ada9b037ce3f8c03930746ddc536ec6749
Sarthak Sunil Dhanke <110623549+Sarztak@users.noreply.github.com>
2026-08-23 11:15:25 -0500

    Add: longest-happy-string - submission-1

---
+++ Data Structures & Algorithms/longest-happy-string/submission-1.py
@@ -0,0 +1,31 @@
+from collections import deque
+from heapq import heapify, heappop, heappush
+# this is similar to the previous problem; with just an extra condition to check 
+# that 3 of a kind are not repeated. 
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        timer = 0
+        ans = ""
+        queue = deque()
+        chars = [(-a, "a"), (-b, "b"), (-c, "c")]
+        heap = [(f, c) for f, c, in chars if f != 0]
+        heapify(heap)
+        while heap or queue:
+            timer += 1
+            if queue and queue[0][2] <= timer:
+                f, c, t = queue.popleft()
+                heappush(heap, (f, c))
+            if heap:
+                f, c = heappop(heap)
+                if ans[-2:] != c * 2:
+                    ans += c
+                    f += 1
+                    if f < 0:
+                        heappush(heap, (f, c))
+                else:
+                    queue.append((f, c, timer + 2))
+            else:
+                break 
+        return ans
+
+        
